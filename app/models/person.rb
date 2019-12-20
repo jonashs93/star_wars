ba65caa386042004a2bef9397495b174e66a80ea
Jonas Silva <jonas_jhs@hotmail.com>
2019-12-20 12:02:59 -0300

    adiciona validacoes e relacionamentos nos models

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ApplicationRecord
+
+  # --- Relationships ---
+
+  belongs_to :planet
+  has_many   :starships
+
+  # --- Validates ---
+
+  validates :name, limit: 100
+  validates :name, :mass, :planet, presence: true
 end
--- app/models/starship.rb
+++ app/models/starship.rb
@@ -1,2 +1,10 @@
 class Starship < ApplicationRecord
+
+  # --- Relationships ---
+
+  has_many :people
+
+  # --- Validates ---
+
+  validates :name, limit: 100, presence: true
 end
